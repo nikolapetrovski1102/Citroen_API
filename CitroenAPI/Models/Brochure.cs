f84829a82e4347674979977fcfda47d1dac7e68b
Петровски Никола <nikola.petrovski.2@students.finki.ukim.mk>
2024-03-19 09:46:23 +0100

    Brochure model

--- CitroenAPI/Models/Brochure.cs
+++ CitroenAPI/Models/Brochure.cs
@@ -1,9 +1,9 @@
-﻿using CitroenAPI.Models;
-
-public class Brochure : Enums
-{
-    public string title { get; set; }
-    public string url { get; set; }
-    public string disposalMode { get; set; }
-}
+﻿namespace CitroenAPI.Models
+{ 
+    public class Brochure : Enums
+    {
+        public string title { get; set; }
+        public string url { get; set; }
+        public string disposalMode { get; set; }
+    }
 }
